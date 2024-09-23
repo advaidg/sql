f77ad59f293c5845816820cd93f0a855c71b7ac7
Advaid Gireesan <73230570+advaidg@users.noreply.github.com>
2024-09-23 17:12:38 +0530

    Update compare.py

--- compare.py
+++ compare.py
@@ -1,17 +1,42 @@
 import json
 from deepdiff import DeepDiff
+import argparse
 
-# Load both JSON files
-with open('output.json', 'r') as f1, open('other_file.json', 'r') as f2:
-    json1 = json.load(f1)
-    json2 = json.load(f2)
+def remove_transaction_number(obj):
+    if isinstance(obj, dict):
+        obj.pop('transactionNumber', None)
+        for key in obj:
+            remove_transaction_number(obj[key])
+    elif isinstance(obj, list):
+        for item in obj:
+            remove_transaction_number(item)
 
-# Compare the two JSON objects
-diff = DeepDiff(json1, json2, ignore_order=True)
+parser = argparse.ArgumentParser(description="Remove 'transactionNumber' and compare two JSON files.")
+parser.add_argument('file1', type=str, help='First JSON file path')
+parser.add_argument('file2', type=str, help='Second JSON file path')
 
-# Check if there is any difference
+args = parser.parse_args()
+
+with open(args.file1, 'r') as f:
+    data1 = json.load(f)
+
+remove_transaction_number(data1)
+
+with open('output1.json', 'w') as f:
+    json.dump(data1, f, indent=2)
+
+with open(args.file2, 'r') as f:
+    data2 = json.load(f)
+
+remove_transaction_number(data2)
+
+with open('output2.json', 'w') as f:
+    json.dump(data2, f, indent=2)
+
+diff = DeepDiff(data1, data2, ignore_order=True)
+
 if not diff:
-    print("The JSON files are identical.")
+    print("The JSON files are identical after removing 'transactionNumber'.")
 else:
     print("The JSON files have differences:")
     print(diff)
